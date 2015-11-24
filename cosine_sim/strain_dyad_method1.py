ca52859b2826a5f19b2a4c278d267c08468de4ca
nopphons <nopphon.s@gmail.com>
2015-11-24 03:12:29 -0800

    add yesPer

--- cosine_sim/strain_dyad_method1.py
+++ cosine_sim/strain_dyad_method1.py
@@ -2,6 +2,7 @@ import csv, re, math, time
 from collections import Counter
 import pandas as pd
 import numpy as np
+from decimal import Decimal, getcontext
 from datetime import date, timedelta
 from nltk import PorterStemmer
 from nltk.corpus import stopwords
@@ -10,6 +11,7 @@ from multiprocessing import Process, Manager
 import sys
 import os
 from datetime import datetime
+from dateutil import parser
 #syntax: python cleantxtparallel.py [csvin] [csvout] [num_chunks] [clean/collate/both]
 #num_chunks = int(sys.argv[3])
 #python -i cosine_strain_parallel.py 'matched_42_83.csv' 'output_matched_42.csv' 16
@@ -59,7 +61,7 @@ def strain_dyad(index,data,start, end,method1):
     with open(output[:-4] + '_' + str(index)+ '_' + str(start) + '-'+ str(end-1) +'.csv', 'wb') as csvtarget:
         chunk_writer = csv.writer(csvtarget)
         chunk_writer.writerow(['strain1','strain2','acquired1','acquired2','strain_same','date_distance','cosine_sim',
-            'review_dist','method1','method_dist','jaccard_sim','jaccard_flavor', 'user_match','med_rec_match', 'dispensary_match'])
+            'review_dist','method1','method_dist','jaccard_sim','jaccard_flavor', 'user_match','med_rec_match', 'dispensary_match','vote_dist','yes_tot'])
         rows = len(data)
         data = data.reset_index()
         if end > rows:
@@ -73,6 +75,7 @@ def strain_dyad(index,data,start, end,method1):
             numstars1 = data['numberofstars'][i]
             review1 = data['reviewtest'][i]
             method1 = data['method2'][i]
+            yesper1 = data['yesPer'][i]
             jaccard1 = set()
             for k in range(1,20):
                 effect = data['effect'+str(k)][i]
@@ -93,6 +96,7 @@ def strain_dyad(index,data,start, end,method1):
                 numstars2 = data['numberofstars'][j]
                 review2 = data['reviewtest'][j]
                 method2 = data['method2'][j]
+                yesper2 = data['yesPer'][j]
                 jaccard2 = set()
                 for k in range(1,20):
                     effect = data['effect'+str(k)][j]
@@ -104,8 +108,7 @@ def strain_dyad(index,data,start, end,method1):
                     if flavor != " ":
                         jaccard_flavor2.add(flavor)
                 strainsame = int(strain1 == strain2) 
-                format = '%m/%d/%Y %I:%M %p'
-                date_distance = abs((datetime.strptime(date2, format) - datetime.strptime(date1, format)).days)
+                date_distance = abs((parser.parse(date1)- parser.parse(date2)).days)
                 cosine = get_cosine(text_to_vector(review1), text_to_vector(review2))
                 review_distance = abs(numstars1-numstars2)
                 type_dist = method1
@@ -121,8 +124,12 @@ def strain_dyad(index,data,start, end,method1):
                 user_match = int(user1 == user2)
                 med_rec_match = int(med_rec1 == med_rec2)
                 dispensary_match = int(acquired1 == acquired2)
+                vote_dist = ' '; yes_tot = ' '
+                if yesper1 != ' ' and yesper2 != ' ':
+                    vote_dist = abs(float(Decimal(yesper1[:-1])-Decimal(yesper2[:-1])))
+                    yes_tot = abs(float(Decimal(yesper1[:-1])+Decimal(yesper2[:-1])))
                 chunk_writer.writerow([strain1,strain2,acquired1,acquired2,strainsame,date_distance,cosine,
-                    review_distance,type_dist,method_dist,jaccard_sim,jaccard_flavor,user_match,med_rec_match,dispensary_match])
+                    review_distance,type_dist,method_dist,jaccard_sim,jaccard_flavor,user_match,med_rec_match,dispensary_match,vote_dist,yes_tot])
     return
 
 
